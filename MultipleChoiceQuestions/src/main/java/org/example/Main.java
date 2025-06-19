2918a0d4d7df9412a8c3d66819f6268f111112d2
Lebohang mofokeng <105319206+LebohangLuuh@users.noreply.github.com>
2025-06-19 21:46:28 +0200

    Revert "method to generate 10 unique random numbers for random questions from the 15 Qs"
    This reverts commit 6750ea1868f06e22157a4266690205c8b4cf6b5d.
    removed mistake code push to main

--- MultipleChoiceQuestions/src/main/java/org/example/Main.java
+++ MultipleChoiceQuestions/src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         QuestionService questionService = new QuestionService();
-        Scanner input = new Scanner(System.in);
 
         // Show welcome message
         questionService.welcomeMessage();
--- MultipleChoiceQuestions/src/main/java/org/example/QuestionInterface.java
+++ MultipleChoiceQuestions/src/main/java/org/example/QuestionInterface.java
@@ -2,7 +2,4 @@ package org.example;
 
 public interface QuestionInterface {
     public void welcomeMessage();
-    public void userAnswer();
-    public void calculateScores();
-    public void randomQuestion();
 }
